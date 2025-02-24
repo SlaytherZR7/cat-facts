f7be5ceddf264316e1b7e25602cf4d1dd4e75ed3
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2025-02-24 11:23:56 -0500

    refactor: simplify App component and update styles for better layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import { useEffect, useState } from 'react'
 import './App.css'
-
-const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
-// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
+import { useCatImage } from './hooks/useCatImage.js'
+import { useCatFact } from './hooks/useCatFact.js'
 
 function App () {
-  const [fact, setFact] = useState('')
+  const { fact, refreshFact } = useCatFact()
+  const { imageUrl } = useCatImage({ fact })
 
-  useEffect(() => {
-    fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then(response => response.json())
-      .then(data => setFact(data.fact))
-  }, [])
+  const handleClick = async () => {
+    refreshFact()
+  }
+
   return (
-    <h1>{fact}</h1>
+    <main>
+      <h1>Fact cat</h1>
+      <button onClick={handleClick}>Get new fact</button>
+      {fact && <p>{fact}</p>}
+      {imageUrl && <img src={imageUrl} alt={`Imagen obtenida usando las primeras tres palabras de ${fact}`} />}
+    </main>
   )
 }
 
